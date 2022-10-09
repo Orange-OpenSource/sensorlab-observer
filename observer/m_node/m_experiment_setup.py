2b1a7f71bc987fa9b355e303ed942b48a66104bc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 08:02:12 +0000

    Adding tarfile member sanitization to extractall()

--- observer/m_node/m_experiment_setup.py
+++ observer/m_node/m_experiment_setup.py
@@ -107,7 +107,26 @@ class Loader:
                     )
                 )
             # decompress the archive
-            archive.extractall(self.temp_directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, self.temp_directory)
 
         # read the manifest
         with open(os.path.join(self.temp_directory, EXPERIMENT_MANIFEST), 'r') as manifest_file:
